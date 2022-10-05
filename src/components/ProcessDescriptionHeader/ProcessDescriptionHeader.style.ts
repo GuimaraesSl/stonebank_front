463de326cffb35c3c26ba07d7720c41b0789e5ce
GuimaraesSl <alsamir.gabriel@gmail.com>
2022-10-05 16:39:08 -0300

    ProcessDescriptionHeader modified margin

--- src/components/ProcessDescriptionHeader/ProcessDescriptionHeader.style.ts
+++ src/components/ProcessDescriptionHeader/ProcessDescriptionHeader.style.ts
@@ -2,11 +2,15 @@ import { makeStyles } from '@material-ui/core'
 import { colors } from '_config/theme'
 
 export const useStyles = makeStyles({
+  
   title: {
+    color:colors.readOnly.light.blackLight,
+    fontSize: '2.18rem',
     fontWeight: 700,
-    marginBottom: 24,
-    lineHeight: '23px',
+    fontFamily: "'Merriweather Sans', sans-serif",
+    padding: '40px 0 15px 0'
   },
+
   subtitle: {
     marginBottom: 4,
     fontWeight: 500,
@@ -14,9 +18,10 @@ export const useStyles = makeStyles({
     lineHeight: '16px',
   },
   description: {
-    color: colors.system.light.neutral,
-    fontWeight: 300,
-    fontSize: '12px',
-    lineHeight: '130%',
+    fontSize: '1.125rem',
+    fontFamily: "'Baloo Bhaina 2', cursive",
+    fontWeight: 400,
+    margin: '0 0 20px 0',
+    color: colors.readOnly.light.blackLight+"95",
   },
 })
